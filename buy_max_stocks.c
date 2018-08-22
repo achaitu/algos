daa86a71cc36887a6011cef65bd3784355bf7b04
Chaitanya <amballachaitanya777@gmail.com>
2018-08-22 16:38:48 +0530

    added version 2

--- buy_max_stocks.c
+++ buy_max_stocks.c
@@ -8,43 +8,29 @@
 
 long int temp;
 long int count = 0;
-long int i = 0;
-long int k;
+long int i,j = 0;
+long int k,past;
 int n; 
 
-void repeat(long int temp, long int k, int*a){
-    
-        printf("%d %d %d",k,temp,a[i]);
-            if (k> 0)
-            {
-                temp = temp + temp/2 +1;
-            }
-            else {
-                temp = temp - temp/2;
-            }
-    
-}
 
 long int buyMaximumProducts(int n, long int k, int* a) {
     while( k> 0 && i<n)
-    {
-            temp = (i+1) /2; 
-            k = k -(temp+1)* a[i];
-            repeat(temp,k,a);
-            if(temp == i){    
-                i +=1;
-                printf("\n yes\n");
-            }
-            else{
-                repeat(temp,k,a);
-            }
+    { 
+            past = k;
+            k = k -(i+1)* a[i];
+            
+            i +=1;
+        //printf("%ld ",k);
     }
-    count = (i*i+1)/2+temp;
-    return count;
-}
-
-
+    temp = past / (long int)a[i-1];
+    //printf("%ld ",temp);
+    count = (i*(i-1))/2+temp;
     
+    
+
+    return count;
+
+}
 
     
 int main() {
